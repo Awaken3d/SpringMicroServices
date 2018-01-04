b79adddb1a21990ffcd97d94806f546145b5f9c9
Nicholas Souris <nisouris@hotmail.com>
2018-01-04 08:16:08 +0200

    added eureka service and reservation client

---
+++ reservation-client/src/main/java/com/souris/main/ReservationClientApplication.java
@@ -0,0 +1,85 @@
+package com.souris.main;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+
+@EnableCircuitBreaker
+@EnableBinding(Source.class)
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ReservationClientApplication {
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
+}
+
+@RestController
+@RequestMapping("/reservations")
+class ReservationGatewayRestController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private Source source;
+	
+	public Collection<String> getReservationNamesFallback() {
+		return new ArrayList<>();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void writeReservation(@RequestBody Reservation r) {
+		Message<String> msg = MessageBuilder.withPayload(r.getReservationName()).build();
+		this.source.output().send(msg);
+	}
+	
+	@HystrixCommand(fallbackMethod = "getReservationNamesFallback")
+	@RequestMapping(method = RequestMethod.GET, value = "/names")
+	public Collection<String> getReservationNames() {
+		ParameterizedTypeReference<Resources<Reservation>> ptr = new ParameterizedTypeReference<Resources<Reservation>>() {};
+		ResponseEntity<Resources<Reservation>> entity =  this.restTemplate.exchange("http://reservation-service/reservations", HttpMethod.GET, null, ptr);
+		return entity.getBody().getContent().stream().map(Reservation :: getReservationName).collect(Collectors.toList());
+	}
+}
+
+class Reservation {
+	private String reservationName;
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+
+}
--- reservation-service/src/main/java/com/souris/main/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/souris/main/ReservationServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -19,6 +20,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 
